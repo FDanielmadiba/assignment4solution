fbf000dd1dd0f107d41bb83a26313da7b69ae989
Nonimo <47617181+FDanielmadiba@users.noreply.github.com>
2020-01-08 18:58:46 -0500

    Add files via upload

---
+++ js/SpeakHello.js
@@ -0,0 +1,1 @@
+(function (window) {   var helloSpeaker={};   helloSpeaker.speak= function speak (name) {          console.log(speakWord + " " + name);}; var speakWord = "Hello"; window.helloSpeaker= helloSpeaker;})(window);
